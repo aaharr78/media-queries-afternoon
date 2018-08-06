4fabca43fc4083c4f2e08533fec0a98b0f65ae46
Aaron Harris <aaronharris@Aarons-MacBook-Pro.local>
2018-08-06 14:37:42 -0600

    first commit

---
+++ media-queries/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="navbar">
+      <div className="nav-title">
+      Start Bootstrap
+      </div>
+      <div className="nav-items">
+        <span className="nav-links">SERVICES</span>
+        <span className="nav-links">PORTFOLIO</span>
+        <span className="nav-links">ABOUT</span>
+        <span className="nav-links">TEAM</span>
+        <span className="nav-links">CONTACT</span>
+        
+      </div>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
